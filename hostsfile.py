8264c7448348a734ab92e4c1b429ad9b30084997
Hugh Nowlan <nosmo@nosmo.me>
2013-10-27 01:24:03 +0100

    Basic support for buffered writes. Not properly enabled

--- hostsfile.py
+++ hostsfile.py
@@ -4,15 +4,24 @@ import os
 
 class HostsFile(object):
 
-    def __init__(self, path):
+    def __init__(self, path, buffer_writes=0):
 
         self.filepath = path
-        self.data = None
+        self.hosts_data = None
+        self.buffer_writes = buffer_writes
+
+        if buffer_writes:
+            self.buffer = []
+        else:
+            self.buffer = None
 
     def __load_data(self):
         linedata = []
         with open(self.filepath, "r") as fileobj:
             for line in fileobj.read().split("\n"):
+                if line.startswith("#"):
+                    continue
+
                 linesplit = line.split()
                 if len(linesplit) == 2:
                     # No comment
@@ -23,7 +32,12 @@ class HostsFile(object):
 
     @staticmethod
     def __render_entry(linesplit):
-        commentstr = "# %s" % linesplit[2] if linesplit[2] else ""
+        commentstr = ""
+
+        if linesplit[2] and not linesplit[2].startswith("#"):
+            commentstr = "#%s" % linesplit[2]
+        elif linesplit[2]:
+            commenstr = linesplit[2]
 
         return "%s %s %s" % (
             linesplit[0],
@@ -31,13 +45,13 @@ class HostsFile(object):
             commentstr
             )
 
-    def __add__(self, ipaddress, bantype="ALL", comment=None):
+    def add(self, ipaddress, bantype="ALL", comment=None):
         self.hosts_data = self.__load_data()
         bantype = "%s:" % bantype
         lineentry = self.__render_entry([bantype, ipaddress, comment])
 
         # if entry is the same, do nothing
-        if lineentry in self.hosts_data:
+        if [bantype, ipaddress, comment] in self.hosts_data:
             return self
 
         # if entry exists, update/replace.
@@ -46,6 +60,34 @@ class HostsFile(object):
                 self.hosts_data[index][0] = bantype
                 self.hosts_data[index][2] = comment
                 return self
+
+        if self.buffer_writes:
+            self.buffer.append(lineentry)
+
+        # finally, just add it
+        if not self.buffer_writes:
+            with open(self.filepath, "a") as fileobj:
+                fileobj.write(
+                    "%s\n" % lineentry
+                    )
+        elif self.buffer_writes and len(self.buffer) >= self.buffer_writes:
+            print "Paging out writes"
+            for entry in self.buffer:
+                with open(self.filepath, "a") as fileobj:
+                    fileobj.write(
+                        "%s\n" % entry
+                        )
+
+        self.hosts_data += [bantype, ipaddress, comment]
+
+        #TODO: return something better
+        return self
+
+    def __add__(self, ipaddress):
+        self.hosts_data = self.__load_data()
+        comment = None
+        bantype = "%s:" % "ALL"
+        lineentry = self.__render_entry([bantype, ipaddress, comment])
 
         # finally, just add it
         with open(self.filepath, "a") as fileobj:
@@ -111,13 +153,15 @@ class HostsFile(object):
 
 class HostsDeny(HostsFile):
 
-    def __init__(self):
-        HostsFile.__init__(self, "/etc/hosts.deny")
+    def __init__(self, buffer_writes=0):
+        HostsFile.__init__(self, "/etc/hosts.deny",
+                           buffer_writes=buffer_writes)
 
 class HostsAllow(HostsFile):
 
-    def __init__(self):
-        HostsFile.__init__(self, "/etc/hosts.allow")
+    def __init__(self, buffer_writes=0):
+        HostsFile.__init__(self, "/etc/hosts.allow",
+                           buffer_writes=buffer_writes)
 
 if __name__ == "__main__":
     # this should be a unit test oh well
